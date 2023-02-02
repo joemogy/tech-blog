a00096077206f498496b1799c95f8f62766154ec
Joseph Mogavero <82268976+joemogy@users.noreply.github.com>
2023-02-02 10:05:37 -0500

    Update index.js

--- Models/index.js
+++ Models/index.js
@@ -1,39 +1,44 @@
-const Blog= require('./Blog');
-const Comment= require('./Comment');
-const User= require('./User');
+const { Model, DataTypes } = require('sequelize');
 
+const sequelize = require('../Config/connection.js');
 
+class Comment extends Model { }
 
-User.hasMany(Blog,{
-    foreignKey:'userName_id',
-    onDelete:'CASCADE'
-})
+Comment.init({
+    id:{
+        type:DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement:true,
+        allowNull:false
+    },
+    commentContent:{
+        type:DataTypes.STRING,
+        allowNull:false
+    },
+    UserName_id:{
+        type:DataTypes.INTEGER,
+        allowNull:false,
+        references:{
+          model: 'user',
+          key:'id'  
+        }
+    },
+    blog_id:{
+        type:DataTypes.INTEGER,
+        allowNull:false,
+        references:{
+            model:'blog',
+            key:'id'
+        }
+    }
+},
+{
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comment',
+  }
+)
 
-Blog.belongsTo(User,{
-    foreignKey:'userName_id'
-})
-
-User.hasMany(Comment,{
-    foreignKey:'userName_id',
-    onDelete:'CASCADE'
-})
-
-Comment.belongsTo(User,{
-    foreignKey:'userName_id'
-})
-
-Blog.hasMany(Comment,{
-    foreignKey:'blog_id',
-    onDelete:'CASCADE'
-})
-
-Comment.belongsTo(Blog,{
-    foreignKey:'blog_id'
-})
-
-module.exports={
-    Comment,
-    Blog,
-    User
-
-};
+module.exports = Comment;
